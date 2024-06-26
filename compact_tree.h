c973e6d360aa7ca27df2be0df8716d0d28561fe5
Niema Moshiri <niemamoshiri@gmail.com>
2024-06-26 13:30:19 -0700

    More foundation

--- compact_tree.h
+++ compact_tree.h
@@ -9,10 +9,11 @@
 #include <cstring>
 #include <ctype.h>
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 // constants
-#define NODE_T std::uint32_t // define node type, which is a fixed-width unsigned integer
+#define CT_NODE_T std::uint32_t // define node type, which is a fixed-width unsigned integer (supports up to 4 billion nodes)
 
 // compact_tree class
 class compact_tree {
@@ -20,8 +21,10 @@ class compact_tree {
         /**
          * compact_tree member variables
          */
-        std::vector<NODE_T> parent;                // `parent[i]` is the parent of node `i`
-        std::vector<std::vector<NODE_T>> children; // `children[i]` is a `vector` containing the children of node `i`
+        std::vector<CT_NODE_T> parent;                            // `parent[i]` is the parent of node `i`
+        std::vector<std::vector<CT_NODE_T>> children;             // `children[i]` is a `vector` containing the children of node `i`
+        std::vector<const char*> label;                           // `label[i]` is the label of node `i`
+        std::unordered_map<const char*, CT_NODE_T> label_to_node; // `label_to_node[label]` is the node labeled by `label`
 
     public:
         /**
@@ -30,6 +33,20 @@ class compact_tree {
          * @param schema The schema of `fn`
          */
         compact_tree(const char* const & fn, char* const & schema);
+
+        /**
+         * Get the label of a node
+         * @param node The node to get the label of
+         * @return The label of `node`
+         */
+        const char* const get_label(CT_NODE_T node);
+
+        /**
+         * Get the label associated with a node
+         * @param label The label
+         * @return The node labeled by `label`, or -1 if not found
+         */
+        CT_NODE_T get_node(const char* const & label);
 };
 
 // compact_tree constructor
@@ -48,5 +65,19 @@ compact_tree::compact_tree(const char* const & fn, char* const & schema) {
     else {
         std::cerr << "ERROR: Invalid schema: " << schema << std::endl; exit(1);
     }
+}
+
+// get label from node
+const char* const compact_tree::get_label(CT_NODE_T node) {
+    return label[node];
+}
+
+// get node from label
+CT_NODE_T compact_tree::get_node(const char* const & label) {
+    auto tmp = label_to_node.find(label);
+    if(tmp == label_to_node.end()) {
+        return (CT_NODE_T)(-1);
+    }
+    return tmp->second;
 }
 #endif
