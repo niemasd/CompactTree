48ab44496a27156ed3c72782aba1e265edb0ed3c
Niema Moshiri <niemamoshiri@gmail.com>
2024-08-07 08:54:00 -0700

    Updated print_distances output to match print_distance_matrix

--- example/print_distances.cpp
+++ example/print_distances.cpp
@@ -15,11 +15,12 @@ int main(int argc, char** argv) {
 
     // find and print all pairwise leaf distances
     std::vector<CT_NODE_T> leaves(tree.leaves_begin(), tree.leaves_end()); size_t num_leaves = leaves.size();
+    std::cout << "u\tv\td" << std::endl;
     for(size_t i = 0; i < num_leaves-1; ++i) {
         CT_NODE_T u = leaves[i]; const std::string & ul = tree.get_label(u);
         for(size_t j = i+1; j < num_leaves; ++j) {
             CT_NODE_T v = leaves[j]; const std::string & vl = tree.get_label(v);
-            std::cout << ul << ", " << vl << ": " << tree.calc_dist(u,v) << std::endl;
+            std::cout << ul << '\t' << vl << '\t' << tree.calc_dist(u,v) << std::endl;
         }
     }
     return 0;
