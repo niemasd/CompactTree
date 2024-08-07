0c3d08cfe273fa4c946fd09347d21b01cfa18994
Niema Moshiri <niemamoshiri@gmail.com>
2024-08-07 15:31:57 -0700

    Added metadata to setup.py

--- setup.py
+++ setup.py
@@ -34,9 +34,29 @@ class build_ext(build_ext_orig):
         subprocess.check_call(['g++', '-shared', 'compact_tree_wrap.o', '-o', 'CompactTree/_compact_tree.so', '-lstdc++'])
         build_ext_orig.run(self)
 
+# package info
 setup(
     name='CompactTree',
     version=compact_tree_version,
+    author='Niema Moshiri',
+    author_email='niemamoshiri@gmail.com',
+    description='CompactTree: Lightweight header-only C++ library for loading and traversing trees',
+    long_description='CompactTree is a header-only C++ library for loading and traversing trees.',
+    long_description_content_type='text/markdown',
+    url='https://github.com/niemasd/CompactTree',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    keywords='tree phylogenetics fast',
     packages=['CompactTree'],
     ext_modules=[Extension('_compact_tree', sources=['CompactTree/compact_tree_wrap.cxx'])],
     cmdclass={'build_ext': build_ext, 'build': build},
